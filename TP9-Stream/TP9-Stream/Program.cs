9362a0e9cc5eed0ae4fb56e7f38adc4ee642f8b7
Claudio <claudioelreloco@gmail.com>
2019-05-30 19:31:28 -0300

    TP9 comenzando

---
+++ TP9-Stream/TP9-Stream/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Helpers;
+
+namespace TP9_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SoporteParaConfiguracion.CrearArchivoDeConfiguracion();
+            SoporteParaConfiguracion.LeerConfiguracion();
+        }
+    }
+}
---
+++ TP9-Stream/TP9-Stream/SoporteParaConfiguracion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    static public class SoporteParaConfiguracion
+    {
+        public static void CrearArchivoDeConfiguracion()
+        {
+            using (BinaryWriter bw = new BinaryWriter(File.Create("Configuracion.dat")))
+            {
+                bw.Write(@"..\Release");
+            }
+        }
+
+        public static string LeerConfiguracion()
+        {
+            string path;
+            using (BinaryReader br = new BinaryReader(File.Open("Configuracion.dat", FileMode.Open)))
+            {
+                path = br.ReadString();
+            }
+            return path;
+        }
+    }
+}
